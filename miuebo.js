286d3318f95f8ae7351f8bbd38418215b8f6dfca
0800-asd <124334621+0800-asd@users.noreply.github.com>
2024-02-17 22:26:07 -0300

    Update miuebo.js

--- miuebo.js
+++ miuebo.js
@@ -1,18 +1,27 @@
-document.body.innerHTML = ''; 
+document.body.innerHTML = '';
+
 document.querySelectorAll('link[rel="stylesheet"]').forEach(function(link) {
     link.parentNode.removeChild(link);
+});
+
 document.title = "0800, CEDUBI & AKMA"; 
+
 var p = document.createElement('p'); 
 p.textContent = "miuebo"; 
 document.body.appendChild(p); 
+
 var img = document.createElement('img'); 
 img.src = "https://i.ibb.co/cb7jrzP/mussi.png";
-imgg.src = "https://i.ibb.co/cb7jrzP/mussi.png"; 
-imggg.src = "https://www.cda.org.ar/img_articulos/images/photos/0800gratis.png"; 
-imgggg.src = "https://www.cda.org.ar/img_articulos/images/photos/0800gratis.png"; 
 document.body.appendChild(img); 
+
+var imgg = document.createElement('img'); 
+imgg.src = "https://i.ibb.co/cb7jrzP/mussi.png"; 
 document.body.appendChild(imgg);
+
+var imggg = document.createElement('img'); 
+imggg.src = "https://www.cda.org.ar/img_articulos/images/photos/0800gratis.png"; 
 document.body.appendChild(imggg);
-document.body.appendChild(imgggg); 
-});
-});
+
+var imgggg = document.createElement('img'); 
+imgggg.src = "https://www.cda.org.ar/img_articulos/images/photos/0800gratis.png"; 
+document.body.appendChild(imgggg);
