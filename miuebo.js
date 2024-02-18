a9bad9c939b8bab6cbbad7e275bf3b71c98bb5e3
0800-asd <124334621+0800-asd@users.noreply.github.com>
2024-02-17 22:16:18 -0300

    Update miuebo.js

--- miuebo.js
+++ miuebo.js
@@ -1,7 +1,7 @@
 document.body.innerHTML = ''; 
 document.title = "0800, CEDUBI & AKMA"; 
 var p = document.createElement('p'); 
-p.textContent = "¡Hola mundo!"; 
+p.textContent = "miuebo"; 
 document.body.appendChild(p); 
 var img = document.createElement('img'); 
 img.src = "https://www.cda.org.ar/img_articulos/images/photos/0800gratis.png"; 
